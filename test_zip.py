0c4c0f3d17360cda9f96fa708dc67f88c76592a5
Lovnish Verma <princelv84@gmail.com>
2026-06-29 20:08:41 +0530

    Remove old test_email.py and test_zip.py files

--- test_zip.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from services.zip_generator import _generate_and_upload_zip
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-with app.app_context():
-    _generate_and_upload_zip('docx', True)
